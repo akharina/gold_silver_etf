fbbe3ac609f233f2b1412858ac3449db666aab6e
Akharina <anastasia.kharina@gmail.com>
2019-10-07 11:31:52 -0400

    merge with master

---
+++ data_compilation_AK.py
@@ -0,0 +1,30 @@
+""" This script compiles data from Alpha Vantage API
+    and transforms the json output into a dataframe and CSV file"""
+import json
+
+# script to get API key
+
+def get_keys(path):
+    with open(path) as f:
+        return json.load(f)
+
+# script to compile data into dataframe per symbol
+
+
+def vantage_point(symbol):
+    """Compiles data per symbol"""
+    api_key = get_keys("/Users/akharina/.secret/alpha_vantage_API.json")['api_key']
+    URL = 'https://www.alphavantage.co/query?'
+    PARAMS = {"function": 'TIME_SERIES_DAILY',
+              "symbol": f'{symbol}',
+              "outputsize": 'full',
+              "apikey": api_key}
+
+    response = requests.get(URL, PARAMS)
+    print(f"{symbol} response code: {response.status_code}")
+    df = pd.DataFrame(response.json()['Time Series (Daily)']).T
+    # df.columns = [f'{symbol}_open', f'{symbol}_high',
+    #               f'{symbol}_low', f'{symbol}_close', f'{symbol}_volume']
+    df['symbol'] = symbol
+    df.to_csv(f"data/raw/{symbol}.csv")
+    return df
